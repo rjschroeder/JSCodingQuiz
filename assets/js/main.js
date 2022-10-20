61bc62586d213ce589e2ff97f4a9f74164abd6ba
rjschroeder <are.jay.schroeder@gmail.com>
2022-10-20 17:31:59 -0500

    started work on changing display in js
    does not work yet, placeholder commit. Need to work on the onClick function

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+let descriptionElement = document.getElementById('desc');
+let finishElement = document.getElementById('finish');
+let startButton = document.getElementById('start');
+
+function startQuiz() {
+    descriptionElement.style.display = 'none';
+    finishElement.style.display = 'none';
+}
+
+//startButton.onclick = startQuiz();
