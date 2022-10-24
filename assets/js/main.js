1ade770cf192a566cb163d66c814b55e3e43bf54
rjschroeder <are.jay.schroeder@gmail.com>
2022-10-24 16:09:11 -0500

    implemented working timer to page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
+let time = 60;
 
 let descriptionElement = document.getElementById('desc');
 let finishElement = document.getElementById('finish');
 let startButton = document.getElementById('start');
+let timerText = document.getElementById('timer');
+let questionText = document.getElementById('question');
+let answersText = document.getElementById('answers');
 
 function startQuiz() {
     descriptionElement.style.display = 'none';
     finishElement.style.display = 'none';
+
+    timerText.textContent = "Time:" + time;
+    timer();
+    
 }
+
+function timer() {setInterval(function() {
+    time -= 1;
+    timerText.textContent = "Time:" + time;
+}, 1000)}
 
 startButton.onclick = startQuiz;
